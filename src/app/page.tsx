9942529a3b6828bcad4f4d28077012c16e11a2bc
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-24 19:22:46 +0900

    コンポーネントを配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import ArticleList from "./components/ArticleList";
+import Aside from "./components/Aside";
 
 export default function Home() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div className="md:flex">
+      <section className="w-full md:w-2/3 flex-col items-center px-3">
+        <ArticleList />
+      </section>
+      <Aside/>
+    </div>
+  );
 }
