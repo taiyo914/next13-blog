a249572ff4fc3b5784e408d118a6c5058726f2bf
taiyo914 <taiyosuzuki0914@icloud.com>
2024-08-24 19:44:44 +0900

    ブログ新規作成ページを作成

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       </div>
       <div>
         <nav className='text-sm font-medium'>
-          <Link href="/article/new" className='bg-orange-300 p-3 rounded-md'>記事を書く</Link>
+          <Link href="/articles/new" className='bg-orange-300 p-3 rounded-md'>記事を書く</Link>
         </nav>
       </div>
     </header>
